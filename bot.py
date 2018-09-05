10bdd9d92c52bee79b74369e0244a32335e758d2
CleverChuk <chubiyke@gmail.com>
2018-09-05 17:05:21 -0400

    added more fields for comment and submission object

--- bot.py
+++ bot.py
@@ -7,8 +7,8 @@ client_secret = os.environ["CLIENT_SECRET"]
 client_id = os.environ["CLIENT_ID"]
 password = os.environ["PASSWORD"]
 username = os.environ["USERNAME"]
-user_agent = "python:evolutionconvo:v1.0.0 (by /u/cleverchuk)"
 
+user_agent = "python:evolutionconvo:v1.0.0 (by /u/%s)" % username
 
 
 class CustomeEncoder(json.JSONEncoder):
@@ -17,25 +17,39 @@ class CustomeEncoder(json.JSONEncoder):
     """
 
     def default(self, o):
-        return  o.__dict__
+        return o.__dict__
 
     def decode_object(self, o):
         pass
 
+
 class Submission:
-    def __init__(self, id, submission):
-        self.id = id
+    """
+        submission object
+    """
+    def __init__(self,submission):
+        self.author_fullname = submission.author_fullname
+        self.id = submission.id
+        self.title = submission.title
+        self.view_count = submission.view_count
+        self.upvote_ratio = submission.upvote_ratio
+        self.ups = submission.ups
+        self.downs = submission.downs
         self.comments = [Comment(comment) for comment in submission.comments]
 
 
 class Comment:
     """
-        Encapsulates all comments
+        comment object
     """
 
     def __init__(self, comment):
+        self.author = "Anonymous" if comment.author == None else comment.author.name
+        self.score = comment.score
+        self.created = comment.created
         self.id = comment.id
         self.body = comment.body
+        self.sentiment = None # todo: update with sentiment analysis
         self.replies = [Comment(c) for c in comment.replies]
 
 
@@ -48,12 +62,12 @@ class RedditBot:
                                   user_agent=user_agent, username=username)
         self.subreddit = self.reddit.subreddit(subreddit)
 
-    def get_all_comments(self):
+    def get_submissions(self):
         """
             get all comments in subreddit.
         """
-        for comment in self.subreddit.comments():
-            yield comment.body
+        return set(comment.submission for comment in self.subreddit.comments())
+        
 
     def get_hot_submissions(self, limit=10):
         """
@@ -72,9 +86,9 @@ class RedditBot:
         submission = self.reddit.submission(id=submission_id)
         submission.comments.replace_more(limit=None)
         with open(filename, "w") as fp:
-            sub = Submission(submission_id, submission)
+            sub = Submission(submission)
             json.dump(sub, fp, separators=(',', ':'),
-                      cls=CustomeEncoder, sort_keys=True, indent=4)
+                      cls=CustomeEncoder, sort_keys=False, indent=4)
 
     def __repr__(self):
         return "RedditBot"
@@ -86,3 +100,6 @@ if __name__ == "__main__":
     submission_id = "9bdwe3"
     bot = RedditBot(subreddit)
     bot.dump_submission_comments(submission_id, filename)
+    # for c in bot.get_submissions():
+    #     print(c)
+    # print(len(bot.get_submissions()))
